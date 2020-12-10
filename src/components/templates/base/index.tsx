fcc0c325c09d7c0af259a954669293038e63deff
whatjackhasmade <jack@noface.co.uk>
2020-12-10 18:58:45 +0000

    Fix import

--- src/components/templates/base/index.tsx
+++ src/components/templates/base/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { ReactQueryDevtools } from 'react-query-devtools';
 
 if (typeof window !== `undefined`) {
   // Make scroll behavior of internal links smooth
