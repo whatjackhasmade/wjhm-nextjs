5b357cb1846952fe3e68784a831604839f6d510b
whatjackhasmade <jack@noface.co.uk>
2020-12-28 11:25:14 +0000

    Update timezone defs

--- src/generated/types/wjhm.tsx
+++ src/generated/types/wjhm.tsx
@@ -16173,6 +16173,8 @@ export enum TimezoneEnum {
   AmericaFortNelson = 'AMERICA_FORT_NELSON',
   /** Glace Bay */
   AmericaGlaceBay = 'AMERICA_GLACE_BAY',
+  /** Godthab */
+  AmericaGodthab = 'AMERICA_GODTHAB',
   /** Goose Bay */
   AmericaGooseBay = 'AMERICA_GOOSE_BAY',
   /** Grand Turk */
@@ -16279,8 +16281,6 @@ export enum TimezoneEnum {
   AmericaNorthDakotaCenter = 'AMERICA_NORTH_DAKOTA_CENTER',
   /** North Dakota - New Salem */
   AmericaNorthDakotaNewSalem = 'AMERICA_NORTH_DAKOTA_NEW_SALEM',
-  /** Nuuk */
-  AmericaNuuk = 'AMERICA_NUUK',
   /** Ojinaga */
   AmericaOjinaga = 'AMERICA_OJINAGA',
   /** Panama */
