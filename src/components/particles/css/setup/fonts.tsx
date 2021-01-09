0ef1ef0167b84016bf3d88c270ac3e3891098a5f
whatjackhasmade <jack@noface.co.uk>
2021-01-09 08:32:30 +0000

    Update file casing for fonts

--- src/components/particles/css/setup/fonts.tsx
+++ src/components/particles/css/setup/fonts.tsx
@@ -3,55 +3,55 @@ import { css } from 'styled-components';
 const exportedCSS = css`
   @font-face {
     font-family: 'SuisseIntl';
-    src: url('/fonts/SuisseIntl-Regular.eot'); /* IE9 Compat Modes */
-    src: url('/fonts/SuisseIntl-Regular.eot?#iefix') format('embedded-opentype'),
-      /* IE6-IE8 */ url('/fonts/SuisseIntl-Regular.woff2') format('woff2'),
-      /* Super Modern Browsers */ url('/fonts/SuisseIntl-Regular.ttf') format('woff'),
-      /* Pretty Modern Browsers */ url('/fonts/SuisseIntl-Regular.ttf') format('truetype');
+    src: url('/fonts/suisseintl-regular.eot'); /* IE9 Compat Modes */
+    src: url('/fonts/suisseintl-regular.eot?#iefix') format('embedded-opentype'),
+      /* IE6-IE8 */ url('/fonts/suisseintl-regular.woff2') format('woff2'),
+      /* Super Modern Browsers */ url('/fonts/suisseintl-regular.ttf') format('woff'),
+      /* Pretty Modern Browsers */ url('/fonts/suisseintl-regular.ttf') format('truetype');
     font-style: normal;
     font-weight: normal;
   }
 
   @font-face {
     font-family: 'SuisseIntl';
-    src: url('/fonts/SuisseIntl-Regular-Italic.eot'); /* IE9 Compat Modes */
-    src: url('/fonts/SuisseIntl-Regular-Italic.eot?#iefix') format('embedded-opentype'),
-      /* IE6-IE8 */ url('/fonts/SuisseIntl-Regular-Italic.woff2') format('woff2'),
-      /* Super Modern Browsers */ url('/fonts/SuisseIntl-Regular-Italic.ttf') format('woff'),
-      /* Pretty Modern Browsers */ url('/fonts/SuisseIntl-Regular-Italic.ttf') format('truetype');
+    src: url('/fonts/suisseintl-regular-italic.eot'); /* IE9 Compat Modes */
+    src: url('/fonts/suisseintl-regular-italic.eot?#iefix') format('embedded-opentype'),
+      /* IE6-IE8 */ url('/fonts/suisseintl-regular-italic.woff2') format('woff2'),
+      /* Super Modern Browsers */ url('/fonts/suisseintl-regular-italic.ttf') format('woff'),
+      /* Pretty Modern Browsers */ url('/fonts/suisseintl-regular-italic.ttf') format('truetype');
     font-style: italic;
     font-weight: normal;
   }
 
   @font-face {
     font-family: 'SuisseIntl';
-    src: url('/fonts/SuisseIntl-Regular-Medium.eot'); /* IE9 Compat Modes */
-    src: url('/fonts/SuisseIntl-Regular-Medium.eot?#iefix') format('embedded-opentype'),
-      /* IE6-IE8 */ url('/fonts/SuisseIntl-Regular-Medium.woff2') format('woff2'),
-      /* Super Modern Browsers */ url('/fonts/SuisseIntl-Regular-Medium.ttf') format('woff'),
-      /* Pretty Modern Browsers */ url('/fonts/SuisseIntl-Regular-Medium.ttf') format('truetype');
+    src: url('/fonts/suisseintl-medium.eot'); /* IE9 Compat Modes */
+    src: url('/fonts/suisseintl-medium.eot?#iefix') format('embedded-opentype'),
+      /* IE6-IE8 */ url('/fonts/suisseintl-medium.woff2') format('woff2'),
+      /* Super Modern Browsers */ url('/fonts/suisseintl-medium.ttf') format('woff'),
+      /* Pretty Modern Browsers */ url('/fonts/suisseintl-medium.ttf') format('truetype');
     font-style: italic;
     font-weight: 500;
   }
 
   @font-face {
     font-family: 'SuisseIntl';
-    src: url('/fonts/SuisseIntl-Regular-Bold.eot'); /* IE9 Compat Modes */
-    src: url('/fonts/SuisseIntl-Regular-Bold.eot?#iefix') format('embedded-opentype'),
-      /* IE6-IE8 */ url('/fonts/SuisseIntl-Regular-Bold.woff2') format('woff2'),
-      /* Super Modern Browsers */ url('/fonts/SuisseIntl-Regular-Bold.ttf') format('woff'),
-      /* Pretty Modern Browsers */ url('/fonts/SuisseIntl-Regular-Bold.ttf') format('truetype');
+    src: url('/fonts/suisseintl-bold.eot'); /* IE9 Compat Modes */
+    src: url('/fonts/suisseintl-bold.eot?#iefix') format('embedded-opentype'),
+      /* IE6-IE8 */ url('/fonts/suisseintl-bold.woff2') format('woff2'),
+      /* Super Modern Browsers */ url('/fonts/suisseintl-bold.ttf') format('woff'),
+      /* Pretty Modern Browsers */ url('/fonts/suisseintl-bold.ttf') format('truetype');
     font-style: italic;
     font-weight: 700;
   }
 
   @font-face {
     font-family: 'SuisseIntl';
-    src: url('/fonts/SuisseIntl-Regular-Black.eot'); /* IE9 Compat Modes */
-    src: url('/fonts/SuisseIntl-Regular-Black.eot?#iefix') format('embedded-opentype'),
-      /* IE6-IE8 */ url('/fonts/SuisseIntl-Regular-Black.woff2') format('woff2'),
-      /* Super Modern Browsers */ url('/fonts/SuisseIntl-Regular-Black.ttf') format('woff'),
-      /* Pretty Modern Browsers */ url('/fonts/SuisseIntl-Regular-Black.ttf') format('truetype');
+    src: url('/fonts/suisseintl-black.eot'); /* IE9 Compat Modes */
+    src: url('/fonts/suisseintl-black.eot?#iefix') format('embedded-opentype'),
+      /* IE6-IE8 */ url('/fonts/suisseintl-black.woff2') format('woff2'),
+      /* Super Modern Browsers */ url('/fonts/suisseintl-black.ttf') format('woff'),
+      /* Pretty Modern Browsers */ url('/fonts/suisseintl-black.ttf') format('truetype');
     font-style: italic;
     font-weight: 900;
   }
