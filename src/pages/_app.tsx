6aa430325d72c8dff8c8a25e8b6398ff4c2a2d09
whatjackhasmade <jack@noface.co.uk>
2021-02-14 18:49:08 +0000

    Update localStorage client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,10 @@ import Router from 'next/router';
 import NProgress from 'nprogress';
 import { ThemeProvider } from 'styled-components';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { persistWithLocalStorage } from 'react-query/persist-localstorage-experimental';
+// @ts-ignore
+import { persistQueryClient } from 'react-query/persistQueryClient-experimental';
+// @ts-ignore
+import { createLocalStoragePersistor } from 'react-query/createLocalStoragePersistor-experimental';
 
 import { GlobalStyle } from 'wjhm';
 import { ThemeDefault } from 'wjhm';
@@ -27,13 +30,19 @@ const DAY = 24 * HOUR;
 const isDevelopment = process.env.NODE_ENV === `development`;
 const maxAge = isDevelopment ? 5 * MINUTE : 1 * DAY;
 
-const client = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: maxAge,
+    },
+  },
+});
 
-persistWithLocalStorage(client, {
-  localStorageKey: `REACT_QUERY_OFFLINE_CACHE`,
-  throttleTime: 1 * SECOND,
-  maxAge,
-  buster: ``,
+const localStoragePersistor = createLocalStoragePersistor();
+
+persistQueryClient({
+  queryClient,
+  persistor: localStoragePersistor,
 });
 
 export default function MyApp(props: MyAppProps) {
